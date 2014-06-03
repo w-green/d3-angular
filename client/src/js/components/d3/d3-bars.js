350e134edff36e86352245943203014649ce49f0
WG <wg@wg.(none)>
2014-06-03 11:54:38 +0100

    Updated to vertical cols from rows

--- client/src/js/components/d3/d3-bars.js
+++ client/src/js/components/d3/d3-bars.js
@@ -8,34 +8,38 @@ angular.module('d3Example', ['d3'])
 					
 					// SVG chart
 
-					var width = 420,
-					    barHeight = 20;
+					var width = 960,
+					    height = 500;
 
-					var x = d3.scale.linear()
-					    .range([0, width]);
+					var y = d3.scale.linear()
+					    .range([height, 0]);
 
 					var chart = d3.select(".chart")
-					    .attr("width", width);
+					    .attr("width", width)
+					    .attr("height", height);
 					
 					d3.tsv('src/app-preprocess/data.tsv', type, function(error, data) {
-						x.domain([0, d3.max(data, function(d) { return d.value; })]);
-						chart.attr("height", barHeight * data.length);
+						y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+						var barWidth = width / data.length;
 
 						var bar = chart.selectAll("g")
 						    .data(data)
 						  .enter().append("g")
-						    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+						    .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+						    // the above plots the starting position for the columns (i = index)
 
 						bar.append("rect")
-						    .attr("width", function(d) { return x(d.value); })
-						    .attr("height", barHeight - 1);
+							.attr("y", function(d) { return y(d.value); })
+							.attr("height", function(d) { return height - y(d.value); })
+							.attr("width", barWidth - 1);
 
 						bar.append("text")
-						    .attr("x", function(d) { return x(d.value) - 3; })
-						    .attr("y", barHeight / 2)
-						    .attr("dy", ".35em")
-						    .text(function(d) { return d.value; });
-					});
+							.attr("x", barWidth / 2)
+							.attr("y", function(d) { return y(d.value) + 3; })
+							.attr("dy", ".75em")
+							.text(function(d) { return d.value; });
+      					});
 
 					function type(d) {
 						d.value = +d.value; // coerce to number
